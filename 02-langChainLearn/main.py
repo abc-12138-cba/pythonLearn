df8b49f40e0d8a241befe84ca648dfd0e93635ca
remi.xie <remi.xie@xgimi.com>
2026-01-08 20:03:08 +0800

    feat: 增加langchain

--- 02-langChainLearn/main.py
+++ 02-langChainLearn/main.py
@@ -5,6 +5,12 @@ from langchain.chat_models import init_chat_model
 from langchain.tools import tool, ToolRuntime
 from langgraph.checkpoint.memory import InMemorySaver
 from langchain.agents.structured_output import ToolStrategy
+import os
+from dotenv import load_dotenv, find_dotenv
+
+load_dotenv(find_dotenv())
+
+os.environ["GOOGLE_API_KEY"] = os.getenv("GOOGLE_API_KEY")
 
 
 # Define system prompt
@@ -38,8 +44,9 @@ def get_user_location(runtime: ToolRuntime[Context]) -> str:
 # Configure model
 model = init_chat_model(
     # todo 替换Gemini 模型名称
-    "claude-sonnet-4-5-20250929",
-    temperature=0
+    "google_genai:gemini-2.5-flash-lite",
+    temperature=0,
+    # max_tokens = 20,
 )
 
 # Define response format
@@ -69,12 +76,12 @@ agent = create_agent(
 config = {"configurable": {"thread_id": "1"}}
 
 response = agent.invoke(
-    {"messages": [{"role": "user", "content": "what is the weather outside?"}]},
+    {"messages": [{"role": "user", "content": "~~你是谁？"}]},
     config=config,
     context=Context(user_id="1")
 )
+print(11, response['structured_response'])
 
-print(response['structured_response'])
 # ResponseFormat(
 #     punny_response="Florida is still having a 'sun-derful' day! The sunshine is playing 'ray-dio' hits all day long! I'd say it's the perfect weather for some 'solar-bration'! If you were hoping for rain, I'm afraid that idea is all 'washed up' - the forecast remains 'clear-ly' brilliant!",
 #     weather_conditions="It's always sunny in Florida!"
@@ -83,12 +90,12 @@ print(response['structured_response'])
 
 # Note that we can continue the conversation using the same `thread_id`.
 response = agent.invoke(
-    {"messages": [{"role": "user", "content": "thank you!"}]},
+    {"messages": [{"role": "user", "content": "~~thank you!"}]},
     config=config,
     context=Context(user_id="1")
 )
 
-print(response['structured_response'])
+print(22, response['structured_response'])
 # ResponseFormat(
 #     punny_response="You're 'thund-erfully' welcome! It's always a 'breeze' to help you stay 'current' with the weather. I'm just 'cloud'-ing around waiting to 'shower' you with more forecasts whenever you need them. Have a 'sun-sational' day in the Florida sunshine!",
 #     weather_conditions=None
